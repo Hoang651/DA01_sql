a2779dbab81e044e74eb3375ff115ffd240dad90
Hoang651 <150848769+Hoang651@users.noreply.github.com>
2023-12-18 08:41:30 +0700

    Create practice_3.sql

---
+++ practice_3.sql
@@ -0,0 +1,57 @@
+--ex1:
+select Name from STUDENTS 
+where Marks > 75
+order by right (Name, 3),ID;
+--ex2:
+select user_id,
+concat (upper (left (name, 1)), lower (right (name, length (name)-1))) as name
+from Users
+order by user_id;
+--ex3:
+SELECT manufacturer,
+CONCAT ('$',ROUND(SUM(total_sales)/1000000,0), ' million') as sale
+FROM pharmacy_sales
+GROUP BY manufacturer
+ORDER BY manufacturer;
+--ex4:
+SELECT  
+EXTRACT(Month from submit_date) as mth,
+product_id as product,
+ROUND(AVG(stars),2) as avg_stars
+FROM reviews
+GROUP BY mth, product_id
+ORDER BY mth, product_id;
+--ex5
+SELECT 
+sender_id,
+COUNT(sender_id) as message_count
+FROM messages
+WHERE EXTRACT(month from sent_date) = 8 and EXTRACT(year from sent_date) = 2022
+GROUP BY sender_id
+ORDER BY message_count desc
+Limit 2
+--ex6:
+select tweet_id 
+from Tweets
+where length (content)>15 ;
+--ex7:
+select activity_date as day, 
+count(distinct(user_id)) as active_users 
+from Activity
+where activity_date between "2019-06-28" and "2019-07-27"
+group by activity_date;
+--ex8:
+count (id) as employee_number
+from employees
+where extract(month from joining_date) between 1 and 7
+and extract(year from joining_date) = '2022';
+--ex9:
+select 
+position ('a' in first_name)
+from worker
+where first_name = 'Amitah';
+--ex10:
+select title as name,
+substring (title, position ('2' in title), 4)
+from winemag_p2
+where country = 'Macedonia';
